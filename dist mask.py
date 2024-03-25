eb7a35de624bccf1502f66a826ef30beaac0256a
盛润华 <shengrunhua@163.com>
2024-03-25 18:29:59 +0800

    Update dist mask.py

--- dist mask.py
+++ dist mask.py
@@ -1,14 +1,14 @@
 import torch
 
-def dist_mask(data,device):
-    data=torch.tensor(data)
-    data=data.to(device)
+def dist_mask(data, device):
+    data = torch.tensor(data)
+    data = data.to(device)
     tile_data = torch.unsqueeze(data, dim=1)
     next_data = torch.unsqueeze(data, dim=-2)
     minus = tile_data - next_data
-    a = -torch.sum(minus**2, -1)
-    dist = torch.exp(a/data.shape[2])
-    dist = dist/torch.sum(dist, 2, keepdims=True)
-    dist=dist+torch.eye(data.shape[1]).to(device)
+    a = -torch.sum(minus ** 2, -1)
+    dist = torch.exp(a / data.shape[2])
+    dist = dist / torch.sum(dist, 2, keepdims = True)
+    dist = dist + torch.eye(data.shape[1]).to(device)
 
     return dist.cpu()  
